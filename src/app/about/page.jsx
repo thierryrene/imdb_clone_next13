b144607821417d5aff16e3e68fb9fa86af518298
thierry <thierryrene@pm.me>
2023-03-06 20:56:49 -0300

    update - create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+export default function pagePage() {
+	return (
+		<div className="max-w-6xl mx-auto space-x-4">
+			<h1 className="text-2xl font-semibold">About</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quod ipsa
+				delectus odit ipsam id nulla incidunt, dolores ratione doloremque omnis
+				aspernatur illum voluptates voluptatem quae dolore. Numquam, architecto
+				repellat.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+				voluptatibus at eaque eligendi culpa numquam necessitatibus accusamus,
+				quia voluptates corporis aliquam, officia nesciunt ab quod impedit rerum
+				sed ratione minima.
+			</p>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+				autem sint reprehenderit provident expedita voluptate repellat, quasi
+				omnis voluptatem obcaecati dignissimos ratione sit consequuntur quos
+				illum non numquam tenetur exercitationem!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+				ducimus eos alias nostrum quia ad laudantium asperiores error, enim quas
+				nemo. Corporis ad est amet perspiciatis voluptates unde omnis sapiente.
+			</p>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
+				minima placeat vero molestiae? Vitae sit repellendus eveniet
+				reprehenderit accusantium, quidem esse voluptate ut odit inventore iure,
+				assumenda perferendis, recusandae
+			</p>
+		</div>
+	);
+}
